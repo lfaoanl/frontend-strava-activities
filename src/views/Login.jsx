6ed338a933cf9d7e74d1dbd8e8199dd45b6f1257
lfaoanl <12160144+lfaoanl@users.noreply.github.com>
2021-07-01 14:33:56 +0200

    Added loading icon for login page

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import includes from 'lodash/includes';
 import Card from '../components/Card';
 import ButtonInput from '../components/inputs/ButtonInput';
 import '../assets/css/page-login.scss';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    const loading = includes(window.location.search, 'code');
+
+    this.state = {
+      loading,
+    };
+  }
+
   render() {
+    const { loading } = this.state;
     return (
       <main className="login">
         <Card>
           <h1>Login</h1>
           <div className="button-container">
-            <ButtonInput label="Login with strava" primary icon="arrow-right" link={window.$strava.authenticateUrl} />
+            <ButtonInput label="Login with strava" primary icon="arrow-right" loading={loading} link={window.$strava.authenticateUrl} />
           </div>
         </Card>
       </main>
