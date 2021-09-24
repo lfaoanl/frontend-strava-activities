c9359b2643a466c8aaca4ea13d28b18fb4e4eb87
lfaoanl <12160144+lfaoanl@users.noreply.github.com>
2021-09-24 09:50:13 +0200

    Login with demo account

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import includes from 'lodash/includes';
 import Card from '../components/Card';
 import ButtonInput from '../components/inputs/ButtonInput';
 import '../assets/css/page-login.scss';
 
 class Login extends Component {
+  static retrieveRefreshToken() {
+    // Login for demo purposes
+    axios.get('https://faanveldhuijsen.nl/novi-refresh-token.php').then(({ data }) => {
+      window.$strava.api.tokenData = data;
+      window.location.reload();
+    });
+  }
+
   constructor(props) {
     super(props);
 
@@ -23,6 +32,7 @@ class Login extends Component {
           <h1>Login</h1>
           <div className="button-container">
             <ButtonInput label="Login with strava" primary icon="arrow-right" loading={loading} link={window.$strava.authenticateUrl} />
+            { includes(window.location.search, 'demo') && <button type="button" className="link" onClick={Login.retrieveRefreshToken}>For NOVI: Login with demo account</button> }
           </div>
         </Card>
       </main>
